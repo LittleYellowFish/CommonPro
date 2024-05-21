d3064cb6de9e97973d8ecf53f6fd869c29c0e852
yle <yule@ziguhonglan.com>
2024-05-21 08:31:37 +0800

    gradle 配置3

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+//import com.example.login.ui.login.LoginActivity;
+import com.example.login.ui.login.LoginActivity;
 import com.example.testapp.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+//        setContentView(R.layout.main_activity);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow();
+//        }
+
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
